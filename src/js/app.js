22d533db806e83e9a9ea1bb5e5f7d9106a5823f0
KuznecovMihail <mihailkuznecov918@gmail.com>
2022-11-14 19:16:11 +1000

    add swiper, nerf font-size li on help

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,56 @@
 import * as flsFunctions from "./modules/functions.js";
 import $ from "jquery";
-import swal from 'sweetalert';
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
+
 
 flsFunctions.isWebp();
+
+const swiper = new Swiper('.swiper', {
+    
+    modules: [Navigation, Pagination],
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1260:{
+            slidesPerView: 3 
+        },
+        850:{
+            slidesPerView: 2 
+        }
+    },
+    centeredSlides: true,
+    grabCursor: true,
+    simulateTouch: true,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true
+    },
+    autoplay:{
+        delay: 5000,
+        disableOnInteraction: true
+    }
+    
+
+});
 
 $(function() {
     $('#nav-icon1').click(function(){
